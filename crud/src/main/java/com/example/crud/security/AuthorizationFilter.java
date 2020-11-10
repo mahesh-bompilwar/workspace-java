f91318417aa9d5953ff0d0f20fa29e5d9e2c4765
Mahesh Bompilwar <mahesh.bompilwar2@gmail.com>
2020-11-10 16:37:17 +0530

    CRUD app created with DB

--- crud/src/main/java/com/example/crud/security/AuthorizationFilter.java
+++ crud/src/main/java/com/example/crud/security/AuthorizationFilter.java
@@ -50,10 +50,8 @@ public class AuthorizationFilter extends BasicAuthenticationFilter {
             if (user != null) {
                 return new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
             }
-
             return null;
         }
             return null;
-
     }
 }
